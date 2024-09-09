e0e4d9e659a98e25f4674cd0f9af28f11e615cf1
Tristan  Tan <tristanyhtan@gmail.com>
2024-09-09 21:10:18 +0000

    working on south or north options

--- story.py
+++ story.py
@@ -1,17 +1,32 @@
 from player import Player
 from wordle import playGame
 
-player1 = Player("", 100)
-player1.name = input("Hello what is your name? ")
+def start_your_trip():
+    player1 = Player("", 100)
+    player1.name = input("Hello what is your name? ")
 
-print(f"Welcome {player1.name} to your adventure")
+    print(f"Welcome {player1.name} to your adventure")
+    print("You wake up in an empty field without any memory of how you got there.")
+    print("Your head is pounding and you see nothing but grass surrounding you.")
+    print("Up ahead you notice a sign that reads 'Graveyard 5 miles North'.") 
+    print("Behind you read a sign that reads 'Sludge Swamp 10 miles South'." )
+    choice = input("Do you want to go up north or down south? (north/south) ")
+    if choice == "north":
+        go_left()
+    if choice == "south":
+        go_right()
+    else:
+        print("Invalid choice. Please choose 'north' or 'south'.")
+        start_your_trip()
 
-print("Do you wish to solve the puzzle?")
+    ## print("On the horizon, you see a rusted gate clouded by a sea of deep fog.")
+    ## print("Do you wish to solve the puzzle?")
+    ## choice = input("Y or N ").upper()
+    ## if choice == "Y":
+    ##     playGame("START")
+    ## elif choice == "N":
+    ##     print("Come again when you're ready.")
+    ## else:
+    ##     print("Invalid input. Please enter Y or N.")
 
-choice = input("Y or N ").upper()
-if choice == "Y":
-    playGame("START")
-elif choice == "N":
-    print("Come again when you're ready.")
-else:
-    print("Invalid input. Please enter Y or N.")
+start_your_trip()
