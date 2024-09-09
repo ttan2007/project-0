6ba7a75a2a7abb29ef4b00eb535bc8617859dff6
Tristan  Tan <tristanyhtan@gmail.com>
2024-09-09 17:18:48 +0000

    used cody for help and started implementing puzzle

--- story.py
+++ story.py
@@ -2,7 +2,16 @@ from player import Player
 from wordle import playGame
 
 player1 = Player("", 100)
-player1.name = input("Hello what is your name")
+player1.name = input("Hello what is your name? ")
 
-print("Weclome " + player1.name + "to your adventre")
-playGame("START")
+print(f"Welcome {player1.name} to your adventure")
+
+print("Do you wish to solve the puzzle?")
+
+choice = input("Y or N ").upper()
+if choice == "Y":
+    playGame("START")
+elif choice == "N":
+    print("Come again when you're ready.")
+else:
+    print("Invalid input. Please enter Y or N.")
